534061e71270b402fd0b08219e2e0c239a4f0ab5
bharathtn <bharathtn12@gmail.com>
2016-09-02 07:21:20 +0530

    Getting Api access

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,6 +4,14 @@ export default Ember.Controller.extend({
     actions: {
         searchT: function () {
             alert("raj");
+            var baseUrl = "https://api.github.com/search/repositories?q=angular";
+            $.getJSON(baseUrl)
+                .done(function (data) {
+                    console.log(data);
+                });
+
+
+
         }
     }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model: function () {
+        return Ember.$.getJSON('https://api.github.com/repos/emberjs/ember.js/pulls');
+    }
+
 });
