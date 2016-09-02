13678dc851ab0a94e2feb3c48d55d06b20187e2b
bharathtn <bharathtn12@gmail.com>
2016-09-02 07:47:48 +0530

    Mobi working

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,11 +4,41 @@ export default Ember.Controller.extend({
     actions: {
         searchT: function () {
             alert("raj");
-            var baseUrl = "https://api.github.com/search/repositories?q=angular";
-            $.getJSON(baseUrl)
-                .done(function (data) {
+            var baseUrl = "https://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Jimi_Hendrix";
+
+            $.ajax({
+                type: "GET",
+                url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Sachin_Tendulkar&callback=?",
+                contentType: "application/json; charset=utf-8",
+                async: false,
+                dataType: "json",
+                success: function (data, textStatus, jqXHR) {
                     console.log(data);
-                });
+                },
+                error: function (errorMessage) {
+                    console.log(errorMessage);
+                }
+            });
+
+
+
+
+            /* var flickerAPI = "https://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Jimi_Hendrix";
+             $.getJSON(baseUrl, {
+                     tags: "mount rainier",
+                     tagmode: "any",
+                     format: "json"
+                 })
+                 .done(function (data) {
+                     console.log(data);
+                     $.each(data.items, function (i, item) {
+                         $("<img>").attr("src", item.media.m).appendTo("#images");
+                         if (i === 3) {
+                             return false;
+                         }
+                     });
+                 });*/
+
 
 
 
