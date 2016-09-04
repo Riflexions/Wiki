19ad41bb6611ed48d00dd76ce2851e1ddf9475f3
bharathtn <bharathtn12@gmail.com>
2016-09-04 12:32:08 +0530

    Modified  chirag

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,29 +1,21 @@
 import Ember from 'ember';
-
+/* globals  $ */
 export default Ember.Controller.extend({
     actions: {
         searchT: function (fieldName) {
-
             var self = this;
             var obj = {};
             $.ajax({
                 type: "GET",
                 url: "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=" + fieldName + "&format=json&callback=?",
-                async: false,
-                dataType: "json",
-                success: function (data, textStatus, jqXHR) {
-                    console.log(data);
-                    obj = data;
-                    self.set("data", data);
-
-                },
-                error: function (errorMessage) {
-                    console.log(errorMessage);
-                }
-            });
-
-
-
+                dataType: "json"
+            }).then(function (data) {
+                console.log(data);
+                obj = data;
+                self.set("data", data);
+            }).fail(function (err) {
+                console.log(err);
+            })
         },
         searchDetails: function (value) {
             var self = this;
@@ -31,20 +23,16 @@ export default Ember.Controller.extend({
                 type: "GET",
                 url: "http://en.wikipedia.org/w/api.php?action=query&prop=extracts|info&exintro&titles=" + value + "&format=json&explaintext&redirects&inprop=url&indexpageids&callback=?",
                 contentType: "application/json; charset=utf-8",
-                async: false,
-                dataType: "json",
-                success: function (data, textStatus, jqXHR) {
-                    console.log(data);
-                    self.set("details", data);
-                    var id = data.query.pageids;
-                    var extract = data.query.pages[id].extract;
-                    self.set("extract", extract);
-                },
-                error: function (errorMessage) {
-                    console.log(errorMessage);
-                }
-            });
+                dataType: "json"
+            }).then(function (data) {
+                console.log(data);
+                self.set("details", data);
+                var id = data.query.pageids[0];
+                var extract = data.query.pages[id].extract;
+                self.set("extract", extract);;
+            }).fail(function (err) {
+                console.log(err);
+            })
         }
-
     }
 });
